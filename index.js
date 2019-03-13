6f055406b2b8e82e12fbbaae4225dfe2b56f234d
mario7746 <m.kennedykavouras@gmail.com>
2019-03-12 22:28:18 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+"use strict"
+
+const inquirer = require("inquirer")
+const chalk = require("chalk")
+
+const response = chalk.bold.blue
+const divide
+
+const resume = require("./resume.json")
+
+const resumePrompts = {
+  type: "list",
+  name: "resumeOptions",
+  message: "What would you like to know about?",
+  choices: [...Object.keys(resume), "Exit"]
+}
+
+function main() {
+  console.log("Hello,My name is Mario Kennedy, welcome to my resume.")
+  resumeHandler()
+}
+
+function resumeHandler() {
+  inquirer.prompt(resumePrompts).then(answer => {
+    if (answer.resumeOptions == "Exit") {
+      return
+    }
+    const option = answer.resumeOptions
+    console.log(response("--------------------------------------"))
+    if(option === 'Experience') {
+      resume[`${option}`].forEach(info => {
+        console.log(response(
+          `|  =>  ${info.company} -- ${info.title}`
+        ))
+
+        console.log(response(
+          `|  =>  ${info.dates}\n`
+        ))
+      })
+    } else if(option === 'Education') {
+      resume[`${option}`].forEach(info => {
+        console.log(response(
+          `|  =>  ${info.school} -- ${info.year}`
+        ))
+
+        console.log(response(
+          `|  =>  ${info.degree}\n`
+        ))
+      })
+    } else {
+      resume[`${option}`]. forEach(info => {
+        console.log(response(`|  => ${info}\n`))
+      })
+    }
+    console.log(response("--------------------------------------"))
+    inquirer
+      .prompt({
+        type: "list",
+        name: "exitBack",
+        message: "Go back or Exit?",
+        choices: ["Back", "Exit"]
+      })
+      .then(choice => {
+        if (choice.exitBack == "Back") {
+          resumeHandler()
+        } else {
+          return
+        }
+      })
+  })
+}
+
+main()
---
+++ readme.md
@@ -0,0 +1,1 @@
+#Mario Kennedy's Resume
